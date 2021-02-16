36aafc58a3accc01b4e50b800ef35cbaf8e29cac
Jolanrensen <j.j.r.rensen@student.tue.nl>
2021-02-16 18:06:55 +0100

    Because of compile issues for Spark 2.4 on Scala 2.11, revert JavaConverters back to JavaConversions

--- kotlin-spark-api/2.4/src/main/kotlin/org/jetbrains/kotlinx/spark/api/Conversions.kt
+++ kotlin-spark-api/2.4/src/main/kotlin/org/jetbrains/kotlinx/spark/api/Conversions.kt
@@ -2,7 +2,7 @@
 
 package org.jetbrains.kotlinx.spark.api
 
-import scala.collection.JavaConverters
+import scala.collection.JavaConversions
 import java.util.*
 import java.util.concurrent.ConcurrentMap
 import scala.collection.Iterable as ScalaIterable
@@ -17,123 +17,123 @@ import scala.collection.mutable.Seq as ScalaMutableSeq
 import scala.collection.mutable.Set as ScalaMutableSet
 
 /**
- * @see JavaConverters.asScalaIterator for more information.
+ * @see JavaConversions.asScalaIterator for more information.
  */
-fun <A> Iterator<A>.asScalaIterator(): ScalaIterator<A> = JavaConverters.asScalaIterator<A>(this)
+fun <A> Iterator<A>.asScalaIterator(): ScalaIterator<A> = JavaConversions.asScalaIterator<A>(this)
 
 /**
- * @see JavaConverters.enumerationAsScalaIterator for more information.
+ * @see JavaConversions.enumerationAsScalaIterator for more information.
  */
-fun <A> Enumeration<A>.asScalaIterator(): ScalaIterator<A> = JavaConverters.enumerationAsScalaIterator<A>(this)
+fun <A> Enumeration<A>.asScalaIterator(): ScalaIterator<A> = JavaConversions.enumerationAsScalaIterator<A>(this)
 
 /**
- * @see JavaConverters.iterableAsScalaIterable for more information.
+ * @see JavaConversions.iterableAsScalaIterable for more information.
  */
-fun <A> Iterable<A>.asScalaIterable(): ScalaIterable<A> = JavaConverters.iterableAsScalaIterable<A>(this)
+fun <A> Iterable<A>.asScalaIterable(): ScalaIterable<A> = JavaConversions.iterableAsScalaIterable<A>(this)
 
 /**
- * @see JavaConverters.collectionAsScalaIterable for more information.
+ * @see JavaConversions.collectionAsScalaIterable for more information.
  */
-fun <A> Collection<A>.asScalaIterable(): ScalaIterable<A> = JavaConverters.collectionAsScalaIterable<A>(this)
+fun <A> Collection<A>.asScalaIterable(): ScalaIterable<A> = JavaConversions.collectionAsScalaIterable<A>(this)
 
 /**
- * @see JavaConverters.asScalaBuffer for more information.
+ * @see JavaConversions.asScalaBuffer for more information.
  */
-fun <A> MutableList<A>.asScalaMutableBuffer(): ScalaMutableBuffer<A> = JavaConverters.asScalaBuffer<A>(this)
+fun <A> MutableList<A>.asScalaMutableBuffer(): ScalaMutableBuffer<A> = JavaConversions.asScalaBuffer<A>(this)
 
 /**
- * @see JavaConverters.asScalaSet for more information.
+ * @see JavaConversions.asScalaSet for more information.
  */
-fun <A> MutableSet<A>.asScalaMutableSet(): ScalaMutableSet<A> = JavaConverters.asScalaSet<A>(this)
+fun <A> MutableSet<A>.asScalaMutableSet(): ScalaMutableSet<A> = JavaConversions.asScalaSet<A>(this)
 
 /**
- * @see JavaConverters.mapAsScalaMap for more information.
+ * @see JavaConversions.mapAsScalaMap for more information.
  */
-fun <A, B> MutableMap<A, B>.asScalaMutableMap(): ScalaMutableMap<A, B> = JavaConverters.mapAsScalaMap<A, B>(this)
+fun <A, B> MutableMap<A, B>.asScalaMutableMap(): ScalaMutableMap<A, B> = JavaConversions.mapAsScalaMap<A, B>(this)
 
 /**
- * @see JavaConverters.dictionaryAsScalaMap for more information.
+ * @see JavaConversions.dictionaryAsScalaMap for more information.
  */
-fun <A, B> Map<A, B>.asScalaMap(): ScalaMap<A, B> = JavaConverters.mapAsScalaMap<A, B>(this)
+fun <A, B> Map<A, B>.asScalaMap(): ScalaMap<A, B> = JavaConversions.mapAsScalaMap<A, B>(this)
 
 /**
- * @see JavaConverters.mapAsScalaConcurrentMap for more information.
+ * @see JavaConversions.mapAsScalaConcurrentMap for more information.
  */
-fun <A, B> ConcurrentMap<A, B>.asScalaConcurrentMap(): ScalaConcurrentMap<A, B> = JavaConverters.mapAsScalaConcurrentMap<A, B>(this)
+fun <A, B> ConcurrentMap<A, B>.asScalaConcurrentMap(): ScalaConcurrentMap<A, B> = JavaConversions.mapAsScalaConcurrentMap<A, B>(this)
 
 /**
- * @see JavaConverters.dictionaryAsScalaMap for more information.
+ * @see JavaConversions.dictionaryAsScalaMap for more information.
  */
-fun <A, B> Dictionary<A, B>.asScalaMap(): ScalaMutableMap<A, B> = JavaConverters.dictionaryAsScalaMap<A, B>(this)
+fun <A, B> Dictionary<A, B>.asScalaMap(): ScalaMutableMap<A, B> = JavaConversions.dictionaryAsScalaMap<A, B>(this)
 
 /**
- * @see JavaConverters.propertiesAsScalaMap for more information.
+ * @see JavaConversions.propertiesAsScalaMap for more information.
  */
-fun Properties.asScalaMap(): ScalaMutableMap<String, String> = JavaConverters.propertiesAsScalaMap(this)
+fun Properties.asScalaMap(): ScalaMutableMap<String, String> = JavaConversions.propertiesAsScalaMap(this)
 
 
 /**
- * @see JavaConverters.asJavaIterator for more information.
+ * @see JavaConversions.asJavaIterator for more information.
  */
-fun <A> ScalaIterator<A>.asKotlinIterator(): Iterator<A> = JavaConverters.asJavaIterator<A>(this)
+fun <A> ScalaIterator<A>.asKotlinIterator(): Iterator<A> = JavaConversions.asJavaIterator<A>(this)
 
 /**
- * @see JavaConverters.asJavaEnumeration for more information.
+ * @see JavaConversions.asJavaEnumeration for more information.
  */
-fun <A> ScalaIterator<A>.asKotlinEnumeration(): Enumeration<A> = JavaConverters.asJavaEnumeration<A>(this)
+fun <A> ScalaIterator<A>.asKotlinEnumeration(): Enumeration<A> = JavaConversions.asJavaEnumeration<A>(this)
 
 /**
- * @see JavaConverters.asJavaIterable for more information.
+ * @see JavaConversions.asJavaIterable for more information.
  */
-fun <A> ScalaIterable<A>.asKotlinIterable(): Iterable<A> = JavaConverters.asJavaIterable<A>(this)
+fun <A> ScalaIterable<A>.asKotlinIterable(): Iterable<A> = JavaConversions.asJavaIterable<A>(this)
 
 /**
- * @see JavaConverters.asJavaCollection for more information.
+ * @see JavaConversions.asJavaCollection for more information.
  */
-fun <A> ScalaIterable<A>.asKotlinCollection(): Collection<A> = JavaConverters.asJavaCollection<A>(this)
+fun <A> ScalaIterable<A>.asKotlinCollection(): Collection<A> = JavaConversions.asJavaCollection<A>(this)
 
 /**
- * @see JavaConverters.bufferAsJavaList for more information.
+ * @see JavaConversions.bufferAsJavaList for more information.
  */
-fun <A> ScalaMutableBuffer<A>.asKotlinMutableList(): MutableList<A> = JavaConverters.bufferAsJavaList<A>(this)
+fun <A> ScalaMutableBuffer<A>.asKotlinMutableList(): MutableList<A> = JavaConversions.bufferAsJavaList<A>(this)
 
 /**
- * @see JavaConverters.mutableSeqAsJavaList for more information.
+ * @see JavaConversions.mutableSeqAsJavaList for more information.
  */
-fun <A> ScalaMutableSeq<A>.asKotlinMutableList(): MutableList<A> = JavaConverters.mutableSeqAsJavaList<A>(this)
+fun <A> ScalaMutableSeq<A>.asKotlinMutableList(): MutableList<A> = JavaConversions.mutableSeqAsJavaList<A>(this)
 
 /**
- * @see JavaConverters.seqAsJavaList for more information.
+ * @see JavaConversions.seqAsJavaList for more information.
  */
-fun <A> ScalaSeq<A>.asKotlinList(): List<A> = JavaConverters.seqAsJavaList<A>(this)
+fun <A> ScalaSeq<A>.asKotlinList(): List<A> = JavaConversions.seqAsJavaList<A>(this)
 
 /**
- * @see JavaConverters.mutableSetAsJavaSet for more information.
+ * @see JavaConversions.mutableSetAsJavaSet for more information.
  */
-fun <A> ScalaMutableSet<A>.asKotlinMutableSet(): MutableSet<A> = JavaConverters.mutableSetAsJavaSet<A>(this)
+fun <A> ScalaMutableSet<A>.asKotlinMutableSet(): MutableSet<A> = JavaConversions.mutableSetAsJavaSet<A>(this)
 
 /**
- * @see JavaConverters.setAsJavaSet for more information.
+ * @see JavaConversions.setAsJavaSet for more information.
  */
-fun <A> ScalaSet<A>.asKotlinSet(): Set<A> = JavaConverters.setAsJavaSet<A>(this)
+fun <A> ScalaSet<A>.asKotlinSet(): Set<A> = JavaConversions.setAsJavaSet<A>(this)
 
 /**
- * @see JavaConverters.mutableMapAsJavaMap for more information.
+ * @see JavaConversions.mutableMapAsJavaMap for more information.
  */
-fun <A, B> ScalaMutableMap<A, B>.asKotlinMutableMap(): MutableMap<A, B> = JavaConverters.mutableMapAsJavaMap<A, B>(this)
+fun <A, B> ScalaMutableMap<A, B>.asKotlinMutableMap(): MutableMap<A, B> = JavaConversions.mutableMapAsJavaMap<A, B>(this)
 
 /**
- * @see JavaConverters.asJavaDictionary for more information.
+ * @see JavaConversions.asJavaDictionary for more information.
  */
-fun <A, B> ScalaMutableMap<A, B>.asKotlinDictionary(): Dictionary<A, B> = JavaConverters.asJavaDictionary<A, B>(this)
+fun <A, B> ScalaMutableMap<A, B>.asKotlinDictionary(): Dictionary<A, B> = JavaConversions.asJavaDictionary<A, B>(this)
 
 /**
- * @see JavaConverters.mapAsJavaMap for more information.
+ * @see JavaConversions.mapAsJavaMap for more information.
  */
-fun <A, B> ScalaMap<A, B>.asKotlinMap(): Map<A, B> = JavaConverters.mapAsJavaMap<A, B>(this)
+fun <A, B> ScalaMap<A, B>.asKotlinMap(): Map<A, B> = JavaConversions.mapAsJavaMap<A, B>(this)
 
 /**
- * @see JavaConverters.mapAsJavaConcurrentMap for more information.
+ * @see JavaConversions.mapAsJavaConcurrentMap for more information.
  */
-fun <A, B> ScalaConcurrentMap<A, B>.asKotlinConcurrentMap(): ConcurrentMap<A, B> = JavaConverters.mapAsJavaConcurrentMap<A, B>(this)
+fun <A, B> ScalaConcurrentMap<A, B>.asKotlinConcurrentMap(): ConcurrentMap<A, B> = JavaConversions.mapAsJavaConcurrentMap<A, B>(this)
 
