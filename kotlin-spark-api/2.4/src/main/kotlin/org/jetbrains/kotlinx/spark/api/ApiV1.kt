9a4c817a1849653c1e8a6fe5b2af3616308b4b98
can wang <can.wang@thoughtworks.com>
2021-08-21 21:47:15 +0800

    fix StructType for nullable list and map field in 2.4

--- kotlin-spark-api/2.4/src/main/kotlin/org/jetbrains/kotlinx/spark/api/ApiV1.kt
+++ kotlin-spark-api/2.4/src/main/kotlin/org/jetbrains/kotlinx/spark/api/ApiV1.kt
@@ -884,7 +884,7 @@ fun schema(type: KType, map: Map<String, KType> = mapOf()): DataType {
             KComplexTypeWrapper(
                 DataTypes.createArrayType(schema(listParam, types), listParam.isMarkedNullable),
                 klass.java,
-                listParam.isMarkedNullable
+                type.isMarkedNullable
             )
         }
         klass.isSubclassOf(Map::class) -> {
@@ -897,7 +897,7 @@ fun schema(type: KType, map: Map<String, KType> = mapOf()): DataType {
                     true
                 ),
                 klass.java,
-                mapValueParam.isMarkedNullable
+                type.isMarkedNullable
             )
         }
         klass.isData -> {
--- kotlin-spark-api/2.4/src/test/kotlin/org/jetbrains/kotlinx/spark/api/ApiTest.kt
+++ kotlin-spark-api/2.4/src/test/kotlin/org/jetbrains/kotlinx/spark/api/ApiTest.kt
@@ -517,6 +517,14 @@ class ApiTest : ShouldSpec({
                 first.someOtherArray shouldBe arrayOf(SomeOtherEnum.C, SomeOtherEnum.D)
                 first.enumMap shouldBe mapOf(SomeEnum.A to SomeOtherEnum.C)
             }
+
+
+            should("Generate encoder correctly with complex enum data class") {
+                val schema = encoder<NullFieldAbleDataClass>().schema()
+                schema.fields().forEach {
+                    it.nullable() shouldBe true
+                }
+            }
         }
     }
 })
@@ -565,4 +573,10 @@ data class ComplexEnumDataClass(
     val someEnumArray: Array<SomeEnum>,
     val someOtherArray: Array<SomeOtherEnum>,
     val enumMap: Map<SomeEnum, SomeOtherEnum>,
+)
+
+
+data class NullFieldAbleDataClass(
+    val optionList: List<Int>?,
+    val optionMap: Map<String, Int>?
 )
