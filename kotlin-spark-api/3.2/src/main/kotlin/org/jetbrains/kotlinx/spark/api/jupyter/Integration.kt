34a0cbdeaf33343d19b87581f01f3ac0f80dcad1
Jolanrensen <jolan.rensen@jetbrains.com>
2022-03-07 14:33:53 +0100

    added dataset html renderer, trying to fix library issues

--- kotlin-spark-api/3.2/src/main/kotlin/org/jetbrains/kotlinx/spark/api/jupyter/Integration.kt
+++ kotlin-spark-api/3.2/src/main/kotlin/org/jetbrains/kotlinx/spark/api/jupyter/Integration.kt
@@ -19,7 +19,14 @@
  */
 package org.jetbrains.kotlinx.spark.api.jupyter
 
+import com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table
+import kotlinx.html.*
+import kotlinx.html.stream.appendHTML
+import org.apache.spark.sql.functions.*
+import org.apache.spark.sql.Dataset
+import org.jetbrains.kotlinx.jupyter.api.HTML
 import org.jetbrains.kotlinx.jupyter.api.libraries.JupyterIntegration
+import org.jetbrains.kotlinx.spark.api.forEach
 
 @OptIn(ExperimentalStdlibApi::class)
 internal class Integration : JupyterIntegration() {
@@ -30,7 +37,6 @@ internal class Integration : JupyterIntegration() {
     private val spark3Version = "3.2.1"
 
     override fun Builder.onLoaded() {
-
 
         dependencies(
             "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion",
@@ -78,5 +84,41 @@ internal class Integration : JupyterIntegration() {
 
 
         }
+
+        // Render Dataset
+        render<Dataset<*>> {
+            HTML(it.toHtml())
+        }
     }
+}
+
+
+private fun <T> Dataset<T>.toHtml(limit: Int = 20, truncate: Int = 20): String = buildString {
+    appendHTML().table {
+        tr {
+            for (header in columns()) th {
+                +header.let {
+                    if (truncate > 0 && it.length > truncate) {
+                        // do not show ellipses for strings shorter than 4 characters.
+                        if (truncate < 4) it.substring(0, truncate)
+                        else it.substring(0, truncate - 3) + "..."
+                    } else {
+                        it
+                    }
+                }
+            }
+        }
+
+        val data = select(col("*"))
+            .takeAsList(limit)
+            .toList()
+
+        for (row in data) tr {
+            for (i in 0 until row.size()) td {
+                +row.get(i).toString()
+            }
+        }
+    }
+
+
 }
