6f6cf611f532db3148638084615a279e4ec7350f
Jolanrensen <jolan.rensen@jetbrains.com>
2022-06-27 12:46:42 +0200

    builds successfully with jcp in :core

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,9 +4,8 @@ import com.vanniktech.maven.publish.JavadocJar.Javadoc
 
 plugins {
     scala
-//    kotlin
     `java-library`
-//    jcp
+    jcp
 
     mavenPublishBase
 }
@@ -34,30 +33,30 @@ dependencies {
 }
 
 
-//tasks.preprocess {
-//    sources.set(sourceSets.main.get().scala.srcDirs)
-//    fileExtensions.set(listOf("java", "scala"))
-//    vars.set(
-//        mapOf(
-//            "scalaCompat" to Versions.scalaCompat,
-//            "spark" to Versions.spark,
-//        )
-//    )
-//}
-//
-//val changeSourceFolder by tasks.registering {
-//    sourceSets.main
-//        .get()
-//        .scala
-//        .setSrcDirs(
-//            listOf(tasks.preprocess.get().target)
-//        )
-//}
-//changeSourceFolder.get().dependsOn(tasks.preprocess)
-//
-//tasks.compileScala
-//    .get()
-//    .dependsOn(changeSourceFolder)
+tasks.preprocess {
+    sources.set(sourceSets.main.get().scala.srcDirs)
+    fileExtensions.set(listOf("java", "scala"))
+    vars.set(
+        mapOf(
+            "scalaCompat" to Versions.scalaCompat,
+            "spark" to Versions.spark,
+        )
+    )
+}
+
+task("changeSourceFolder") {
+    sourceSets.main
+        .get()
+        .scala
+        .setSrcDirs(
+            listOf(tasks.preprocess.get().target)
+        )
+}.dependsOn(tasks.preprocess)
+
+
+tasks.compileScala
+    .get()
+    .dependsOn(tasks.preprocess)
 
 mavenPublishing {
     configure(JavaLibrary(Javadoc()))
