213260b100a3ae10435c299af5339e4844c2f924
Jolanrensen <jolan.rensen@jetbrains.com>
2022-07-14 14:27:13 +0200

    version 1.1.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val project = "1.2.0-SNAPSHOT"
+    const val project = "1.1.1"
     const val groupID = "org.jetbrains.kotlinx.spark"
     const val kotlin = "1.7.10"
 
