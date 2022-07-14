c9c29e13594be9db6a66ff0c68c614e7d9ebd472
Jolanrensen <jolan.rensen@jetbrains.com>
2022-07-14 14:23:56 +0200

    added --scan to gradle build actions, attempt to fix release docs

--- buildSrc/src/main/kotlin/Projects.kt
+++ buildSrc/src/main/kotlin/Projects.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 import org.gradle.api.Project
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.kotlin.dsl.support.delegates.ProjectDelegate
